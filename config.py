0ebb0c347ab9f1818c2d93ffd09db0fadcccd933
bezchuvstv <bezchuvstv404@gmail.com>
2026-07-28 15:10:12 +0300

    Update config.py with personal configuration data
    Added configuration details including BOT_TOKEN, ADMIN_ID, API_ID, API_HASH, and other parameters.

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
-BOT_TOKEN = ""
-ADMIN_ID = 
-YOOMONEY_WALLET = ""
-API_ID = 
-API_HASH = ""
+# ==========================================
+# КОНФИГУРАЦИЯ (ЛИЧНЫЕ ДАННЫЕ)
+# ==========================================
+
+BOT_TOKEN = "8254882046:AAHw5R5UA2ObBopYg0A3JN3E5KaMbtleZ6Y"
+ADMIN_ID = 5451203188
+API_ID = 37929729
+API_HASH = "a9b0048cb977e7efe52c0e9ebef901e1"
+YOOMONEY_WALLET = "410018555840601"
+DB_NAME = 'shop.db'
+CHANNEL_ID = -1003449054946
+ADMIN_CHAT_ID = ADMIN_ID
+ADMINS = [5451203188]  # Главный админ
