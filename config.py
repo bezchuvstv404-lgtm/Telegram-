58acedf2b5b193289a406acf51d8c8606fa29ccb
bezchuvstv <bezchuvstv404@gmail.com>
2026-08-04 15:51:07 +0300

    Clear sensitive data in config.py
    Removed sensitive configuration values and replaced them with empty placeholders.

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 # ==========================================
 
 BOT_TOKEN = ""
-ADMIN_ID = 5451203188
-API_ID = 37929729
-API_HASH = "a9b0048cb977e7efe52c0e9ebef901e1"
-YOOMONEY_WALLET = "4100119486619208"
+ADMIN_ID = 
+API_ID = 
+API_HASH = ""
+YOOMONEY_WALLET = ""
 DB_NAME = 'shop.db'
-CHANNEL_ID = -1003449054946
-ADMIN_CHAT_ID = 5451203188
-ADMINS = [5451203188]
+CHANNEL_ID = 
+ADMIN_CHAT_ID = 
+ADMINS = []
 
 # ==========================================
 # БАЗА СТРАН (100+ САМЫХ ПОПУЛЯРНЫХ)
