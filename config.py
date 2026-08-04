318040822244b9dfd03aae324c9a4b3d584dc048
bezchuvstv <bezchuvstv404@gmail.com>
2026-08-04 15:46:21 +0300

    Update config.py # Conflicts: #	config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 # КОНФИГУРАЦИЯ (ЛИЧНЫЕ ДАННЫЕ)
 # ==========================================
 
-BOT_TOKEN = ""
-ADMIN_ID = 
-API_ID = 
-API_HASH = ""
-YOOMONEY_WALLET = ""
+BOT_TOKEN = "8254882046:AAFfPJjPGafPs2Me_ApykXj1yoWG4rrJSbY"
+ADMIN_ID = 5451203188
+API_ID = 37929729
+API_HASH = "a9b0048cb977e7efe52c0e9ebef901e1"
+YOOMONEY_WALLET = "4100119486619208"
 DB_NAME = 'shop.db'
-CHANNEL_ID = 
-ADMIN_CHAT_ID = 
-ADMINS = []
+CHANNEL_ID = -1003449054946
+ADMIN_CHAT_ID = 5451203188
+ADMINS = [5451203188]
 
 # ==========================================
 # БАЗА СТРАН (100+ САМЫХ ПОПУЛЯРНЫХ)
